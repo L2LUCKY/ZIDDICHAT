797a358e728f8b7a42eee685c1a9d87540c89e23
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-09-18 17:22:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "bfee9f2d599eb92dbe32867e20538140"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "6057446393:AAEGiGnP6pkfmMF5iAmrYZn87RkT6Ga_hLw")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Lucky:Lucky@atlascluster.f7lck9c.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "5247304559"
+SUPPORT_GRP = "LuckyxSupport"
+UPDATE_CHNL = "Luckyxupdate"
+OWNER_USERNAME = "itz_Lucky_Raja"
 
