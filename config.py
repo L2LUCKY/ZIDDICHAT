c73039d141f714fa67ca68575b1ddb997535987b
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-09-18 17:21:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "24461319"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "bfee9f2d599eb92dbe32867e20538140"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6057446393:AAEGiGnP6pkfmMF5iAmrYZn87RkT6Ga_hLw")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Lucky:Lucky@atlascluster.f7lck9c.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = "6664582540"
 SUPPORT_GRP = "HEROKUFREECC"
 UPDATE_CHNL = "ALLTYPECC"
